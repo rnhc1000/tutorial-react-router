910e7a36bf47ff486fb9caa0eb99ebc8803c1834
Ricardo Ferreira <rnhc1000@hotmail.com>
2023-10-06 19:15:22 -0300

    Links without full page reload

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
 
+import { Link } from 'react-router-dom'
 import './App.css'
 
 function App() {
-  
+
 
   return (
+
     <>
+      <nav
+        style={{
+          borderBottom: "solid 1px",
+          paddingBottom: "1rem",
+        }}
+      >
+        <Link to="/invoices">Invoices</Link> |{" "}
+        <Link to="/expenses">Expenses</Link>
+      </nav>
       <h1>Bookkeeper!</h1>
     </>
   )
