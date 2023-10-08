b4a5c76bd57be8961171c683f0ad6948a12f41fa
Ricardo Ferreira <rnhc1000@hotmail.com>
2023-10-08 12:23:01 -0300

    Active Lnks

--- routes/Invoices/index.tsx
+++ routes/Invoices/index.tsx
@@ -1,27 +1,23 @@
-import { Link, Outlet } from "react-router-dom";
+import './styles.css';
+import { NavLink, Outlet } from "react-router-dom";
 import { getInvoices } from '../../src/data';
 
 export default function Invoices() {
     const invoices = getInvoices();
     return (
-        <div style={{ display: "flex" }}>
-        <nav
-          style={{
-            borderRight: "solid 1px",
-            padding: "1rem"
-          }}
-        >
-          {invoices.map((invoice) => (
-            <Link
-              style={{ display: "block", margin: "1rem 0" }}
-              to={`/invoices/${invoice.number}`}
-              key={invoice.number}
-            >
-              {invoice.name}
-            </Link>
-          ))}
-        </nav>
-        <Outlet />
-      </div>
+        <div className="menu">
+            <nav className="nav-bar">
+                {invoices.map((invoice) => (
+                    <NavLink
+                        className={({ isActive }) => isActive ? "dblock nav-red" : "dblock nav-blue"}
+                        to={`/invoices/${invoice.number}`}
+                        key={invoice.number}
+                    >
+                        {invoice.name}
+                    </NavLink>
+                ))}
+            </nav>
+            <Outlet />
+        </div>
     );
-  }
+}
